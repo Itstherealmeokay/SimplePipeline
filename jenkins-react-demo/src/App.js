3aa1bc0cc59218d1c6946cc87aa9d701ed6e6980
Chingz Com <gohchingye@gmail.com>
2026-05-26 17:22:15 +0800

    Setup

---
+++ jenkins-react-demo/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div style={{ 
+      textAlign: 'center', 
+      marginTop: '50px',
+      fontFamily: 'Arial' 
+    }}>
+      <h1>Hello World!</h1>
+      <p>Jenkins Pipeline Demo</p>
+    </div>
+  );
+}
+
+export default App;
